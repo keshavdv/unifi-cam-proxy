235a938c86fd15dcf649a02997272f1074cb8bd7
Keshav Varma <keshavdv@gmail.com>
2020-06-19 00:29:04 -0700

    Create docker-compose example

---
+++ docker/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if [ ! -z "${RTSP_URL:-}" ] && [ ! -z "${HOST}" ] && [ ! -z "${TOKEN}" ]; then
+  echo "Using RTSP streom from $RTSP_URL"
+  exec unifi-cam-proxy --host "$HOST" --name "${NAME:-unifi-cam-proxy}" --mac "${MAC:-'AA:BB:CC:00:11:22'}" --cert /client.pem --token "$TOKEN" rtsp -s "$RTSP_URL"
+fi
+
+exec "$@"
